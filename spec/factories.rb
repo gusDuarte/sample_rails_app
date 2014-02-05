a38bcd93c56e7c9246d3b8703b7d2c4cca2fbe59
Gustavo Duarte <gduarte@activitycentral.com>
2014-02-05 18:10:59 -0200

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,17 @@
+#FactoryGirl.define do
+#  factory :user do
+#    name     "Gustavo Duarte"
+#    email    "gus.duarte@gmail.com"
+#    password "foobar"
+#    password_confirmation "foobar"
+#    end
+#  end
+
+FactoryGirl.define do
+  factory :user do
+    name     "Michael Hartl"
+    email    "michael@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
